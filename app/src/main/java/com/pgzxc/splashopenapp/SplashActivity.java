464237ab92cf887fb16a39de03e4c6c5dcbc93c3
pgzxc <827489398@qq.com>
2018-01-22 09:44:01 +0800

    Initial commit

---
+++ app/src/main/java/com/pgzxc/splashopenapp/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.pgzxc.splashopenapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 秒开启动页
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
